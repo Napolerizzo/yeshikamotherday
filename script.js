d6b38fe58a7792c8a46f85954bd5e040ebaf3023
Napolerizzo <147470586+Napolerizzo@users.noreply.github.com>
2026-05-10 21:05:23 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById("loveBtn");
-const message = document.getElementById("message");
-
-btn.addEventListener("click", () => {
-  message.classList.remove("hidden");
-});
